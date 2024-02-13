587f21750d57251e73da44a5e4d1964c8b16f904
Zach Thompson <zach@allotropic.com>
2024-02-13 05:48:34 -0500

    Add winit and wgpu

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use winit::{event_loop::EventLoop, window::WindowBuilder};
+
+fn main() -> Result<(), impl std::error::Error> {
+    let event_loop = EventLoop::new().unwrap();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    let instance = wgpu::Instance::default();
+    let surface = instance.request_adapter(&wgpu::RequestAdapterOptions {
+    }).await
+
+    event_loop.run(move |event, window| {
+        println!("{event:?}");
+    })
 }
